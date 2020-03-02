ffaf17ab2142df796b14b793931d91f8c31d04cd
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2020-03-02 23:43:44 +0000

    Create and add FeedViewController to MainTabBarController

---
+++ twitter-clone-lab/ViewController/FeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedViewController.swift
+//  twitter-clone-lab
+//
+//  Created by Filipe Jorge on 02/03/2020.
+//  Copyright © 2020 Filipe Jorge. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController
+{
+    // MARK: - Lifecycle
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGray
+    }
+}
