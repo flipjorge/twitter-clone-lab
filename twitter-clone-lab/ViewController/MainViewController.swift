0bcd2bb5982ae26eb9114debf07a2b6ef8359578
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2020-03-07 20:55:28 +0000

    Create MainViewController

---
+++ twitter-clone-lab/ViewController/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  twitter-clone-lab
+//
+//  Created by Filipe Jorge on 07/03/2020.
+//  Copyright © 2020 Filipe Jorge. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController
+{
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor.appTheme.blue.rgb
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        //
+        
+    }
+}
